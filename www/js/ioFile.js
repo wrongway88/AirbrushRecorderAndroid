0800f502eb77dd8a38993a300c5c50826cc85c20
Manuel <McDobusch@gmx.net>
2013-07-21 14:49:38 +0200

    added debug output

--- www/js/ioFile.js
+++ www/js/ioFile.js
@@ -1,11 +1,15 @@
 var ioFile = {
 	fail: function(error)
 	{
+		alert("fail");
+	
 		console.log(error.code);
 	},
 
 	gotFileWriter: function(writer)
 	{
+		alert("gotFileWriter");
+	
 		writer.onwriteend = function(evt)
 		{
             console.log("contents of file now 'some sample text'");
@@ -24,17 +28,20 @@ var ioFile = {
 
 	gotFileEntry: function(fileEntry)
 	{
+		alert("gotFile");
 		fileEntry.createWriter(gotFileWriter, fail);
 
 	},
 
 	gotFS: function(fileSystem)
 	{
+		alert("gotFS");
 		fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, gotFileEntry, fail);
 	},
 
 	initialize: function()
 	{
+		alert("initialize");
 		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
 	}
 }

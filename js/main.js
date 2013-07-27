4d977777b50ec33ca5296b1232ef3db8e66cac2f
Manuel <McDobusch@gmx.net>
2013-07-27 15:34:58 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -12,11 +12,33 @@ var app = {
 			  'message: ' + error.message + '\n');
 	},
 
+	gotFS: function(fileSystem)
+	{
+		alert("gotFS");
+		fileSystem.root.getFile("foo.txt", {create: true, exclusive: false}, gotFileEntry, onError);
+	},
+	
+	gotFileEntry: function(fileEntry)
+	{
+		alert("gotFileEntry");
+		fileEntry.createWriter(gotFileWriter, onError);
+	},
+	
+	gotFileWriter: function(writer)
+	{
+		alert("gotFileWriter");
+		writer.write("moin");
+	},
+	
     initialize: function()
 	{
+		alert("initialize");
+	
         var self = this;
-		var options = {timeout: 30000};
+		var options = {timeout: 60000};
+		//navigator.geolocation.getCurrentPosition(onSuccess, onError);
 		watchID = navigator.geolocation.watchPosition(this.onSuccess, this.onError, options);
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, onError);
     }
 };
 
